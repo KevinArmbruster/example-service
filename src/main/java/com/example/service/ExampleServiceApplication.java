55be57ed20ece81330f01f62b664106195ad22b3
Kevin Armbruster <kevinarmbruster2013@gmail.com>
2021-02-21 23:05:10 +0100

    remove unnecessary files & renaming - part 2

--- src/main/java/com/example/service/ExampleServiceApplication.java
+++ src/main/java/com/example/service/ExampleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseserviceApplication {
+public class ExampleServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServiceApplication.class);
+    }
 
 }
